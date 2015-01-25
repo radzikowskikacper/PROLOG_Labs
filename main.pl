ccc3869d74c51ee93f7a6356e47a16be12aaad3c
Kacper Radzikowski <radzikowskikacper@gmail.com>
2015-01-25 01:57:45 +0100

    perform action

--- main.pl
+++ main.pl
@@ -1,17 +1,19 @@
-	%main(List):-
+%main(List):-
 	%goals_achieved(List, [on(b2, b7), on(b7, b4), on(b4, p1), on(b3, p3), clean(p2), clean(p4), clean(b2), clean(b3)]).
-main(Goal, Goals, Rest):-
-	choose_goal(Goal, Goals, Rest, [on(b2, b7), on(b7, b4), on(b4, p1), on(b3, p3), clean(p2), clean(p4), clean(b2), clean(b3)]).
- 
-choose_goal(Goal, [Goal | Rest], Rest, InitState) :- 
-	not( member(Goal, InitState) ), !.
-	
-choose_goal(Goal, [ _ | Rest], NewRest, InitState) :- 
-	choose_goal(Goal, Rest, NewRest, InitState).
 
+%main(Goal, Goals, Rest):-
+	%choose_goal(Goal, Goals, Rest, [on(b2, b7), on(b7, b4), on(b4, p1), on(b3, p3), clean(p2), clean(p4), clean(b2), clean(b3)]).
+
+main(Action, State):-
+	perform_action([on(b1, p1), clean(p2), clean(p3), clean(p4), clean(b1)], Action, State).
 	
+% niedeterministyczny  wybór celu 
+choose_goal(Goal, [Goal | Rest], Rest, InitState) :- 
+	not( member(Goal, InitState) ).
 	
-	
+choose_goal(Goal, [ PrevGoal | Rest ], [ PrevGoal | NewRest ], InitState) :- 
+	choose_goal(Goal, Rest, NewRest, InitState).
+% END niedeterministyczny wybór celu	
 	
 achieves(move(X, _, Y), on(X, Y)).
 
@@ -33,10 +35,7 @@ requires(move(X, Y, Z), [clean(X / on(X, Y), clean(Z / nil))], _):-
 requires(move(X, Y, Z), [clean(X / on(X, Y / on(Y, Target))), clean(Z / nil)], Target):-
   not(atomic(Y)).
 
-
-
-
-	
+  
 goals_achieved([], _).
 
 goals_achieved([G|R], State) :-
@@ -44,8 +43,10 @@ goals_achieved([G|R], State) :-
 	goals_achieved(R, State).
 
 check_goal(G, State):-
+	writeln('procedura check_goal->'),
 	parse(G, GList),
-	%writeln(GList),
+	write('Lista: '),
+	writeln(GList),
 	subset(GList, State). 
 
 parse(nill, []).	
@@ -58,6 +59,14 @@ parse(clean(X/C), [clean(X) | Rest]) :-
 parse(on(X, Y/C),  [on(X, Y) | Rest]) :-
 	parse(C, Rest).
 
+
+perform_action(PrevState, move(X, Y, Z), NextState) :- 
+	delete(PrevState, clean(Z), MidS1),
+	delete(MidS1, on(X, Y), MidS2),
+	append([on(X, Z), clean(Y)], MidS2, NextState).	
+	
+	
+	
 gen(Max, Max1):-
 	current_predicate(tempmax/1),!,
 	tempmax(Max2),
